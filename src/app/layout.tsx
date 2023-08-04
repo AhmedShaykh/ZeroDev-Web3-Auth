e35f86755ad9f434eef36cc434e6097af126f502
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-08-04 15:08:41 +0500

    Add Shadcn UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-zinc-800 text-slate-300`}
+        className={`${inter.className} bg-zinc-800 text-slate-200`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Button } from "@/Components/button";
 
 const Home = () => {
     return (
-        <div>
+        <div className="flex justify-center items-center h-screen">
+            <Button
+                className="bg-white hover:bg-black text-black hover:text-white text-xl p-6 font-bold"
+            >
+                Connect Wallet
+            </Button>
         </div>
     )
 };
